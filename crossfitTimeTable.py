8972b5e5edfa4adcb0d740ab0aa55e422a2cda72
Ripudaman Flora <ripudamanflora@Ripudamans-MacBook-Pro.local>
2016-12-31 16:58:48 -0500

    some changes in the code

--- crossfitTimeTable.py
+++ crossfitTimeTable.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib2,re, requests, os
 
+
+from prettytable import PrettyTable
+x = PrettyTable()
+
+
 '''
-r = requests.get('https://fantasy.premierleague.com/a/team/my', auth=('singh.pavi9@hotmail.com', 'suttapride891'))
-print(r.status_code)
+x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+x.add_row(["Adelaide",1295, 1158259, 600.5])
+
+print x.get_string(fields=["City name", "Population"])
+
 '''
 
 html = urllib2.urlopen("https://app.wodify.com/Schedule/PublicCalendarListView.aspx?tenant=3920").read()
@@ -19,18 +27,29 @@ FinalData = []
 for row in table_body.find_all("tr"):
     for stat in row.find_all("td", attrs={'class':['TableRecords_EvenLine', 'TableRecords_OddLine']}):
         dictContent ={}
+        dict2Content = {}
         for spn in stat.find_all("span"):
-            if spn.has_attr("title"):
+            if spn.has_attr("class"):
+                    if 'h3' in spn["class"]:
+                        x.field_names = [str(spn.contents[0]), str(spn.contents[2])]
+                        print x.get_string()
+                        dictContent["Day"] = str(spn.contents[0])
+                        dictContent["Date"] = str(spn.contents[2])
+            elif spn.has_attr("title"):
                 if 'Olympic Weightlifting' in spn["title"]:
                     if re.match(r'^[0-9]', spn["title"][0]):
-                            dictContent["Title"] = str(spn["title"])
+                        x.add_row([str(spn["title"]),""])
+                        dictContent["Title"] = str(spn["title"])
                 elif 'CrossFit' in spn["title"]:
                     if re.match(r'^[0-9]', spn["title"][0]):
+                        x.add_row([str(spn["title"]),""])
                         dictContent["Title"] = str(spn["title"])
                 elif 'Open Gym' in spn["title"]:
                     if re.match(r'^[0-9]', spn["title"][0]):
+                        x.add_row([str(spn["title"]),""])
                         dictContent["Title"] = str(spn["title"])
                 elif 'Athletic Conditioning' in spn["title"]:
+                    x.add_row([str(spn["title"]),""])
                     if re.match(r'^[0-9]', spn["title"][0]):
                         dictContent["Title"] = str(spn["title"])
         if len(dictContent) >0:
