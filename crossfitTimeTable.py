85e37c8732e8da77f2ed0c4643da902f6ba81c6c
Ripudaman Flora <eripflo@gmail.com>
2017-01-02 19:47:00 -0500

    Deleting dead code

--- crossfitTimeTable.py
+++ crossfitTimeTable.py
@@ -5,15 +5,6 @@ import urllib2,re, requests, os
 from prettytable import PrettyTable
 x = PrettyTable()
 
-
-'''
-x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-x.add_row(["Adelaide",1295, 1158259, 600.5])
-
-print x.get_string(fields=["City name", "Population"])
-
-'''
-
 html = urllib2.urlopen("https://app.wodify.com/Schedule/PublicCalendarListView.aspx?tenant=3920").read()
 soup = BeautifulSoup(html,"lxml")
 
